0857d3276cf44ae5726818a96203494faa7ff3cb
Shirsendu Bairagi <shirsendu2001@gmail.com>
2024-09-22 16:52:03 +0530

    feat: cli arguments added, config added, printing algorithm added

--- scripts/unascii.mjs
+++ scripts/unascii.mjs
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 
+/* import { createJiti } from "jiti";
+
+const jiti = createJiti(import.meta.url)
+const { runMain } = jiti("../src/cli.ts"); */
+
 import jiti from "jiti";
 
 const { runMain } = jiti(import.meta.url)("../src/cli");
